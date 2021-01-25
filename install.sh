f9677f0f6f3623a8dc732f077291441b8d3126c7
nhatminh100416 <75288955+nhatminh100416@users.noreply.github.com>
2021-01-25 09:08:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
-sudo apt-get update &&  echo y | sudo apt-get install dante-server nginx && sudo sh -c "sudo echo '
+#!/bin/bash
+interfaces=`ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}'`
+sudo apt-get update &&  echo y | sudo apt-get install dante-server && sudo sh -c "sudo echo '
 logoutput: stderr
 user.privileged: root
 user.unprivileged: nobody
 user.libwrap: nobody
-internal: ens4  port = 9966
-external: ens4
+internal: $interfaces  port = 9966
+external: $interfaces
 method: username none
 client pass {
   from: 0.0.0.0/0 to: 0.0.0.0/0
 }
 pass {
   from: 0.0.0.0/0 to: 0.0.0.0/0
-} ' > /etc/danted.conf" && sudo systemctl enable danted nginx && sudo service danted start &&  sudo service nginx start
+} ' > /etc/danted.conf" && sudo systemctl enable danted && sudo service danted start &&  sudo service nginx start
